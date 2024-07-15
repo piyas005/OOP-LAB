add1be73a103c4f33d0430ac47f0758ff4c3e941
piyas <minipiyas@gmail.com>
2024-07-15 19:00:32 +0600

    witing the stating code of the lab5

---
+++ lab5/src/main/java/oopd5/App.java
@@ -0,0 +1,11 @@
+package oopd5;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    Person person = new Person("John", 25);
+    // System.out.println(person.getName());
+    // System.out.println(person.getAge());
+    }
+}
